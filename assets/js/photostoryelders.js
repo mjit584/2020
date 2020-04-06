553fc9e7c939dcc6fe329f75afec8d8664d20d66
Tamara Rice <tamara@sliceinteractive.com>
2020-04-06 11:45:41 -0400

    fixing bugs

--- assets/js/photostoryelders.js
+++ assets/js/photostoryelders.js
@@ -14,17 +14,20 @@ var btn = document.getElementById("myBtn");
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
+  document.body.classList.add('modal-open');
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  document.body.classList.remove('modal-open');
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    document.body.classList.remove('modal-open');
   }
 }
 
